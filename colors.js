87762effa697b7a77aead349d16f7ded94e90ef6
Caleb Pendergraft <cackmed@gmail.com>
2019-12-04 09:41:28 -0800

    added .include

--- colors.js
+++ colors.js
@@ -3,19 +3,22 @@ class Colors {
         this.favorites = [];
     }
     addColor(color) {
-        this.favorites.push(color);
+        if(!this.hasColor(color)) 
+            this.favorites.push(color);
     }
     hasColor(color) {
-        for(let i = 0; i < this.favorites.length; i++) {
-            if(this.favorites[i] === color) {
-                return console.log(true);
-            }
-            else return console.log(false);
-        }
+        return this.favorites.includes(color);
+    //     for(let i = 0; i < this.favorites.length; i++) {
+    //         if(this.favorites[i] === color) {
+    //             return console.log(true);
+    //         }
+    //         else return console.log(false);
+    //     }
     }
 }
   
 const calebsColors = new Colors();
+calebsColors.addColor('blue');
 calebsColors.addColor('blue');
 calebsColors.addColor('red');
 calebsColors.addColor('green');
